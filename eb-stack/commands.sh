5572d0406db7a43b26c1008a099c6255a71c5b7f
edwardmercado <edwardallenmercado@gmail.com>
2021-03-11 01:02:02 +0800

    Refactor EB stack, Add CDN stack

---
+++ eb-stack/commands.sh
@@ -0,0 +1,5 @@
+# package cfn
+aws cloudformation package --template ./template.yaml --s3-bucket challenge1-bucket --output json > packaged-template.json --region us-east-1 --profile apper
+
+# deploy cfn
+aws cloudformation deploy --template ./packaged-template.json --stack-name emercado-tier-1-eb-app-stack  --capabilities CAPABILITY_IAM --region us-east-1 --profile apper
